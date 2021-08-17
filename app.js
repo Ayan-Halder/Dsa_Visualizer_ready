b6caf85f2fead36f4a9beec0392051b1a3c3bea5
Ayan Halder <ayanhalder194@gmail.com>
2021-08-17 13:27:23 +0530

    edited app.js file

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ button.addEventListener("mouseout", function(){
     button.style.background = "white";
     button.style.transform = "scale(1)";
     button.innerText = "click here";
+    button.style.boxShadow = "0px 0px 0px transparent";
     console.log("Now mouse is out")
 });
 button.addEventListener("mouseover", function(){
     button.style.background = "red";
     button.innerText = "clicked";
-    button.style.transform = "scale(1.5)";
+    button.style.transform = "scale(1.1)";
+    button.style.boxShadow = "0px 1px 5px black";
     console.log("Now mouse is on the button")
 });
 // document.getElementById("button").addEventListener("mouseout", function(){
