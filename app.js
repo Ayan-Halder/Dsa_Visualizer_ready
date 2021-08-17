ae16cd80ceef008945f913f164bb3baec23e6020
Ayan Halder <ayanhalder194@gmail.com>
2021-08-17 11:33:06 +0530

    edited app.js file

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ function displaydate(){
 }
 
 let button = document.getElementById('button')
+let head = document.getElementById('h2')
 
 
-button.addEventListener("click", displaydate);
+button.addEventListener("click", function(){
+    displaydate();
+    head.innerText = "The button is clicked"
+});
 button.addEventListener("mouseout", function(){
     button.style.background = "white";
     button.style.transform = "scale(1)";
+    button.innerText = "click here";
     console.log("Now mouse is out")
 });
 button.addEventListener("mouseover", function(){
     button.style.background = "red";
+    button.innerText = "clicked";
     button.style.transform = "scale(1.5)";
     console.log("Now mouse is on the button")
 });
