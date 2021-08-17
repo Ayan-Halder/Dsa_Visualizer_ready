94bdfdf37b0a8dc9eb703e735b30d0f42f6dff9b
Ayan Halder <ayanhalder194@gmail.com>
2021-08-17 09:29:21 +0530

    edited app.js file

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ function displaydate(){
     console.log(mydate);
 }
 
+let button = document.getElementById('button')
 
-document.getElementById("button").addEventListener("click", displaydate);
-document.getElementById("button").addEventListener("mouseout", function(){
+
+button.addEventListener("click", displaydate);
+button.addEventListener("mouseout", function(){
+    button.style.background = "white";
+    button.style.transform = "scale(1)";
     console.log("Now mouse is out")
 });
+button.addEventListener("mouseover", function(){
+    button.style.background = "red";
+    button.style.transform = "scale(1.5)";
+    console.log("Now mouse is on the button")
+});
+// document.getElementById("button").addEventListener("mouseout", function(){
+//     console.log("Now mouse is out")
+// });
+// document.getElementById("button").addEventListener("mouseout", function(){
+//     console.log("Now mouse is out")
+// });
