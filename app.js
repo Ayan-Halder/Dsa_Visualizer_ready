19fb50f393bf8523c06277f9acfc5a67b6e26f07
Ayan Halder <ayanhalder194@gmail.com>
2021-08-18 11:53:28 +0530

    Edited all files

--- app.js
+++ app.js
@@ -4,17 +4,43 @@ function displaydate(){
     console.log(mydate);
 }
 
-let button = document.getElementById('btn1')
+let button = document.getElementById('arraygen')
+let sortit = document.getElementById('sortit')
 let head = document.getElementById('h2')
 let array = document.getElementsByClassName('arr')
+
+
+
+function bblSort(arr){
+    for(var i = 0; i < arr.length; i++){
+      for(var j = 0; j < ( arr.length - i -1 ); j++){
+        if(arr[j] > arr[j+1]){
+          var temp = arr[j]
+          arr[j] = arr[j + 1]
+          arr[j+1] = temp
+          
+        }
+      } 
+    }
+    printArray(arr);
+   }
+     
+
+function printArray(a){
+    for(i = 0; i<10; i++) {
+        var id1='arr'+i;
+        document.getElementById(id1).style.width = a[i]+'px';
+        document.getElementById('arr'+i).innerText = a[i];
+    }
+}
+
 
 function randomarray(){
     let a = [];
     for(i = 0; i<10; i++) {
         a[i] = Math.floor(Math.random() * 1000);
-        document.getElementById('arr'+i).style.width = a[i]+'px';
-        document.getElementById('arr'+i).innerText = a[i];
     }
+    printArray(a);
 
     console.log(a);
 }
@@ -24,6 +50,36 @@ button.addEventListener("click", function(){
     randomarray();
     head.innerText = "The button is clicked"
 });
+
+sortit.addEventListener("click", function(){
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // button.addEventListener("mouseout", function(){
 //     button.style.background = "white";
 //     button.style.transform = "scale(1)";
