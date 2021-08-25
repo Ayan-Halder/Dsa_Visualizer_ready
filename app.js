f2a3abbd1a18a35c1cc0451b0863f4ed8bbeab16
ayanhalder194 <ayanhalder194@gmail.com>
2021-08-25 22:04:33 +0530

    nothing

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ async function insertionSort(arr)
 async function bblSort(arr){
     disableAll();
     for(var i = 0; i < arr.length; i++){
-      for(var j = 0; j < ( arr.length - i -1 ); j++){
+    for(var j = 0; j < ( arr.length - i -1 ); j++){
         if(arr[j] > arr[j+1]){
                 var temp = arr[j];
                 arr[j] = arr[j + 1];
